16732c1e5c0a3649d3107ded284358129072c798
TwistRU <pavlyuk.rs@students.dvfu.ru>
2023-05-08 21:57:27 +1000

    Change get request to API

--- src/requets.ts
+++ src/requets.ts
@@ -1,13 +1,14 @@
+import axios from "axios";
+
 export const getData = async (path: string) => {
-  return fetch(`http://localhost:3000/data/${path}`
-    , {
+    const baseURL = "http://158.160.96.8/api/";
+  return await axios.get(baseURL+path, {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
       }
-    }
-  )
-  .then(response => response.json())
+  })
+  .then(response => response.data)
   .then((responseData) => {
     //console.log(responseData);
     return responseData;
